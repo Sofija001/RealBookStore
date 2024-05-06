933abc2582bb135e343e4519ebdb9f18e044d5ba
Sofija001 <128833511+Sofija001@users.noreply.github.com>
2024-05-06 19:50:57 +0200

    Revert "fixed comment section sql injection and xss vulnerabilities"
    This reverts commit da0869bc8f65b3a6517d02d1dda0851426291eef.

--- src/main/java/com/urosdragojevic/realbookstore/repository/CommentRepository.java
+++ src/main/java/com/urosdragojevic/realbookstore/repository/CommentRepository.java
@@ -12,7 +12,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-import java.sql.*;
 
 @Repository
 public class CommentRepository {
@@ -27,15 +26,11 @@ public class CommentRepository {
     }
 
     public void create(Comment comment) {
-        //String query = "insert into comments(bookId, userId, comment) values (" + comment.getBookId() + ", " + comment.getUserId() + ", '" + comment.getComment() + "')";
-        String query = "insert into comments(bookId, userId, comment) values (" + comment.getBookId() + ", " + comment.getUserId() + ", ?)";
+        String query = "insert into comments(bookId, userId, comment) values (" + comment.getBookId() + ", " + comment.getUserId() + ", '" + comment.getComment() + "')";
 
         try (Connection connection = dataSource.getConnection();
-             //Statement statement = connection.createStatement();
-             PreparedStatement statement = connection.prepareStatement(query);
-
+             Statement statement = connection.createStatement();
         ) {
-            statement.setString(1, comment.getComment());
             statement.execute(query);
         } catch (SQLException e) {
             e.printStackTrace();
